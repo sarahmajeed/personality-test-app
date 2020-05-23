fc60239e71d32cca9acb0de1372825a0f54d9eb7
Sarahmdawood <sarahmajeed368@gmail.com>
2020-05-23 21:19:56 +0500

    Adding a separate button class in a separate class

---
+++ lib/answer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class Answer extends StatelessWidget {
+
+  final Function selectHandler;
+
+  Answer(this.selectHandler);
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child:  RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text("Answer 1"), 
+        onPressed: selectHandler,),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 void main(){
   runApp(MyApp());
@@ -39,11 +40,10 @@ class _MyAppState extends State<MyApp>{
         ),
         body: Column(children: [
           Question(questions[_questionIndex]),
-          RaisedButton(child: Text("Answer 1"), onPressed: _answerQuestion,),
-          RaisedButton(child: Text("Answer 2"), onPressed: () => print("Anwer 2 chosen")),
-          RaisedButton(child: Text("Answer3"),onPressed: () {
-            print("Answer 3 chosen");
-          },),
+         Answer(_answerQuestion),
+         Answer(_answerQuestion),
+         Answer(_answerQuestion),
+         
           ],),
         ),
         )
